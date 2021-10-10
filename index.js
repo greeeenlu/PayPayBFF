540cd7c1af1de0f5012e13f17d4086150a63eb87
Green Lu <42085638+greeeenlu@users.noreply.github.com>
2021-10-10 15:40:15 +0800

    feat: defined field resolver

--- index.js
+++ index.js
@@ -3,24 +3,39 @@ const { ApolloServer, gql } = require("apollo-server");
 const users = [
   {
     id: 1,
+    password: "1234",
     email: "asdf@paypay.com",
     name: "user-1",
-    level: 0,
-    reviewIds: [1, 2]
+    role: 0,
+    reviewIds: [1, 2],
+    revieweeIds: [2, 3]
   },
   {
     id: 2,
+    password: "5678",
     email: "qwer@paypay.com",
     name: "user-2",
-    level: 0,
-    reviewIds: [3, 4]
+    role: 0,
+    reviewIds: [3, 4],
+    revieweeIds: [1]
   },
   {
     id: 3,
+    password: "9012",
     email: "zxcv@paypay.com",
     name: "user-3",
-    level: 0,
-    reviewIds: [5, 6]
+    role: 0,
+    reviewIds: [5, 6],
+    revieweeIds: []
+  },
+  {
+    id: 4,
+    password: "9999",
+    email: "admin@paypay.com",
+    name: "admin",
+    role: 1024,
+    reviewIds: [],
+    revieweeIds: [1, 2, 3]
   }
 ];
 
@@ -30,42 +45,42 @@ const reviews = [
     reviewerId: 2,
     revieweeId: 1,
     feedback: "good!",
-    createdAt: "2021/10/10"
+    createdAt: "2018-10-22T01:40:14.941Z"
   },
   {
     id: 2,
     reviewerId: 2,
     revieweeId: 1,
     feedback: "good!",
-    createdAt: "2021/10/10"
+    createdAt: "2018-10-22T01:40:24.941Z"
   },
   {
     id: 3,
     reviewerId: 1,
     revieweeId: 2,
     feedback: "good!",
-    createdAt: "2021/10/10"
+    createdAt: "2018-10-22T01:40:34.941Z"
   },
   {
     id: 4,
     reviewerId: 1,
     revieweeId: 2,
     feedback: "good!",
-    createdAt: "2021/10/10"
+    createdAt: "2018-10-22T01:41:14.941Z"
   },
   {
     id: 5,
     reviewerId: 1,
     revieweeId: 3,
     feedback: "good!",
-    createdAt: "2021/10/10"
+    createdAt: "2018-10-22T01:42:14.941Z"
   },
   {
     id: 6,
     reviewerId: 2,
     revieweeId: 3,
     feedback: "good!",
-    createdAt: "2021/10/10"
+    createdAt: "2018-10-22T01:43:14.941Z"
   }
 ];
 
@@ -75,8 +90,9 @@ const typeDefs = gql`
     id: ID!
     email: String!
     name: String
-    level: Int
+    role: Int
     reviews: [Review]
+    reviewees: [User]
   }
 
   type Review {
@@ -88,18 +104,35 @@ const typeDefs = gql`
   }
 
   type Query {
-    hello: String
     user: User
     employees: [User]
   }
 `;
 
+//helper functions
+const filterReviewsByIds = (reviewIds) =>
+  reviews.filter((review) => reviewIds.includes(review.id));
+const filterRevieweesByIds = (revieweeIds) =>
+  users.filter((user) => revieweeIds.includes(user.id));
+
+const findUserByUserId = (userId) =>
+  users.find((user) => user.id === Number(userId));
+
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (root, args, context) => "Hello world!",
-    user: (root, args, context) => users[0],
+    user: (root, args, context) => users[3],
     employees: (root, args, context) => users
+  },
+  User: {
+    reviews: (parent, args, context) =>
+      filterReviewsByIds(parent.reviewIds || []),
+    reviewees: (parent, args, context) =>
+      filterRevieweesByIds(parent.revieweeIds || [])
+  },
+  Review: {
+    reviewer: (parent, args, context) => findUserByUserId(parent.reviewerId),
+    reviewee: (parent, args, context) => findUserByUserId(parent.revieweeId)
   }
 };
 
